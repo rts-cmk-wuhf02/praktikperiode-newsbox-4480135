269bf18a817b251828f5c23331034e76c0eb4bd4
4480135 <4480135@RTS-365.dk>
2020-03-24 20:14:26 +0100

    Finish basic implementation of archive removal.

--- src/js/archive.js
+++ src/js/archive.js
@@ -12,6 +12,11 @@ archivedNews.forEach(function(e) {
     
     for(let i = 0; i < e.items.length; i++) {
         const itemClone = templateNewsItemDOM.content.cloneNode(true);
+        // Needed for removal
+        itemClone.querySelector(".news-item").setAttribute("data-id", i);
+        itemClone.querySelector(".news-item").setAttribute("data-category", e.title);
+
+        // Visual information
         itemClone.querySelector(".news-item-headline").innerText = e.items[i].headline;
         itemClone.querySelector(".news-item-body").innerText = e.items[i].body;
         itemClone.querySelector(".news-item-link").href = e.items[i].link;
@@ -23,6 +28,36 @@ archivedNews.forEach(function(e) {
     dropdownContainerDOM.appendChild(dropdownClone);
 });
 
+
+const trashButtonsDOM = document.querySelectorAll(".button-trash");
+
+trashButtonsDOM.forEach(function(e) {
+    e.addEventListener("click", function(e) {
+        const compPath = e.composedPath();
+
+        for(let i = 0; i < compPath.length; i++) {
+            if(compPath[i].classList.contains("news-item")) {
+                // Remove item from data archive
+                const dataID = compPath[i].getAttribute("data-id");
+                const dataCategory = compPath[i].getAttribute("data-category");
+
+                let newArchivedNews = archivedNews;
+                for(let n = 0; n < newArchivedNews.length; n++) {
+                    if(newArchivedNews[n].title == dataCategory) {
+                        newArchivedNews[n].items.splice(dataID, 1);
+                        break;
+                    }
+                }
+                sessionStorage.setItem("archived-news", JSON.stringify(newArchivedNews));
+
+
+                // Remove item from current document
+                compPath[i].remove(); // TODO: Implement fade-out
+                break;
+            }
+        }
+    });
+});
 
 /*
 Archived item structure:
@@ -40,6 +75,4 @@ Archived item structure:
         ]
     }
 ]
-
-
 */
