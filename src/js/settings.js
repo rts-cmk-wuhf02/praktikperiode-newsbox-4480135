c2cbeed7b4cb28aeaf48e74925c91038433123d7
4480135 <4480135@RTS-365.dk>
2020-03-25 11:06:51 +0100

    Add initial set of settings.

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,6 +4,9 @@ const togglesDOM = document.querySelectorAll(".option-checkbox");
 togglesDOM.forEach(function(e) {
     if(sessionStorage.getItem(e.id) == "true") {
         e.click();
+    } else if(!sessionStorage.getItem(e.id)) {
+        sessionStorage.setItem(e.id, "true");
+        e.click();
     }
 
     e.addEventListener("change", function(e) {
