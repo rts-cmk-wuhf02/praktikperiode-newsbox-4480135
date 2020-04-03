35b0678b7635ccf7124c4bef5fdafa9ad1269ee1
aam051102 <ahlgreenmadsen@gmail.com>
2020-04-03 12:11:12 +0200

    WIP on google auth.

--- functions/google-auth.js
+++ functions/google-auth.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google").Strategy;
 
 exports.handler = function(event, context, callback) {
+    console.log(event, context);
     callback(null, {
         statusCode: 200,
         body: "Hello, World"
