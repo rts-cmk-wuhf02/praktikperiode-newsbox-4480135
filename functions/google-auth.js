e9f5d45f2e27fe02c14c165a9f84bf78bb53e4fb
aam051102 <ahlgreenmadsen@gmail.com>
2020-04-03 12:17:50 +0200

    WIP on google auth.

--- functions/google-auth.js
+++ functions/google-auth.js
@@ -1,8 +1,30 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google").Strategy;
 
+passport.initialize();
+passport.session();
+
+passport.use(new GoogleStrategy({
+        returnURL: 'https://distracted-montalcini-ba1430.netlify.com/settings',
+        realm: 'https://distracted-montalcini-ba1430.netlify.com/'
+    },
+    function(identifier, profile, done) {
+        process.nextTick(function () {
+        
+        profile.identifier = identifier;
+            return done(null, profile);
+        });
+    }
+));
+
 exports.handler = function(event, context, callback) {
-    console.log(event, context);
+    passport.authenticate("google", {
+        scope: [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email"
+        ]
+    })
+
     callback(null, {
         statusCode: 200,
         body: "Hello, World"
