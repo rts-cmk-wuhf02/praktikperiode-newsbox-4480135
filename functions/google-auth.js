498cd18cf3fb09d88551726d16fcfc5da04875a9
aam051102 <ahlgreenmadsen@gmail.com>
2020-04-03 12:04:52 +0200

    WIP on google auth.

--- functions/google-auth.js
+++ functions/google-auth.js
@@ -1,15 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google").Strategy;
-const serverless = require("serverless-http");
-const express = require('express');
-const cookieParser = require("cookie-parser");
 
-const app = express();
-  
-app.get(`https://distracted-montalcini-ba1430.netlify.com/.netlify/functions/google-auth`, function(req, res) {
-    console.log("passed");
-    res.render("Test");
-});
-
-
-exports.handler = serverless(app);
+exports.handler = function(event, context, callback) {
+    callback(null, {
+        statusCode: 200,
+        body: "Hello, World"
+    });
+}
